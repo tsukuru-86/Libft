4dab07b72b1b735a5aa9cb9ba0973ba911fe7eea
Tsukuru <tsukuru501@gmail.com>
2024-04-23 19:24:11 +0900

    The first time merge?

---
+++ libft/ft_bzero.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsukuru <tsukuru@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 14:33:57 by tsukuru           #+#    #+#             */
+/*   Updated: 2024/04/22 15:35:15 by tsukuru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void ft_bzero(void *s, size_t n)
+{
+    unsigned char *p;
+    size_t i;
+
+    p = s;
+    i = 0;
+    while(i < n)
+    {
+        p[i] = 0;
+        i++;
+    }
+}
+
+// #include <stdio.h>
+// #include <strings.h>
+
+// void print_bytes(const void *s, size_t n) {
+//     const unsigned char *p = s;
+//     for (size_t i = 0; i < n; i++) {
+//         printf("%02x ", p[i]);
+//     }
+//     printf("\n");
+// }
+
+// int main() {
+//     char str[] = "Hello, World!";
+//     size_t len = sizeof(str);
+
+//     printf("元の文字列のバイト: ");
+//     print_bytes(str, len);
+
+//     ft_bzero(str, len);
+
+//     printf("ft_bzero適用後のバイト: ");
+//     print_bytes(str, len);
+
+//     return 0;
+// }
