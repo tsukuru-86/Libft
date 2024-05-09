1a2e17fb734c33cb34ad91c6c720dad7d31edaa6
Tsukuru <tsukuru501@gmail.com>
2024-05-09 20:37:05 +0900

    Je suis fatigue

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsukuru <tsukuru@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/09 20:11:47 by tsukuru           #+#    #+#             */
+/*   Updated: 2024/05/09 20:35:37 by tsukuru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    size_t all_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+    char *space = (char *)malloc(all_len);
+    char *temp = space;
+
+    if (!space)
+    return NULL;
+    
+    while (*s1)
+    {
+        *temp++ = (char)*s1++;
+    }
+    while (*s2)
+    {
+        *temp++ = (char)*s2++;
+    }
+    *temp = '\0';
+    
+    return space;
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char *a = "12345";
+//     char *b = "67890";
+    
+//     char *résultat = ft_strjoin(a, b);
+//     printf("Le total est '%s'\n", résultat);
+// }
