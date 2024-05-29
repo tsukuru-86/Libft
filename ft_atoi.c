cba8e50be516cc2f89894eafebac765b90b5869a
tkomai <tkomai@student.42tokyo.jp>
2024-05-29 20:12:55 +0900

    I changed little bit

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tkomai <tkomai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 19:20:16 by tkomai            #+#    #+#             */
-/*   Updated: 2024/05/28 19:45:08 by tkomai           ###   ########.fr       */
+/*   Updated: 2024/05/29 18:27:25 by tkomai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,27 @@ int	ft_atoi(const char *str)
 {
 	int	result;
 	int	sign;
+	int digit;
 
 	result = 0;
 	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
-	{
 		str++;
-	}
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-		{
 			sign = -1;
-		}
 		str++;
 	}
 	while (*str >= '0' && *str <= '9')
 	{
+		digit = *str - '0';
+
+		if (sign == 1 && (result > (2147483647 - digit) / 10))
+			return 2147483647;
+		if (sign == -1 && (result > (2147483648 - digit) / 10))
+			return -2147483648;
+
 		result = result * 10 + (*str - '0');
 		str++;
 	}
