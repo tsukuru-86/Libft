7f5d3ed18a3df517b19343d37b17e848c23e46db
tkomai <tkomai@student.42tokyo.jp>
2024-05-29 20:29:24 +0900

    I fixes atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,13 +12,30 @@
 
 #include "libft.h"
 
+int	output(const char *str, int sign)
+{
+	int	result;
+	int	digit;
+
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		digit = *str - '0';
+		if (sign == 1 && (result > (2147483647 - digit) / 10))
+			return (2147483647);
+		if (sign == -1 && (result > (2147483648 - digit) / 10))
+			return (-2147483648);
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result);
+}
+
 int	ft_atoi(const char *str)
 {
 	int	result;
 	int	sign;
-	int digit;
 
-	result = 0;
 	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
@@ -28,27 +45,14 @@ int	ft_atoi(const char *str)
 			sign = -1;
 		str++;
 	}
-	while (*str >= '0' && *str <= '9')
-	{
-		digit = *str - '0';
-
-		if (sign == 1 && (result > (2147483647 - digit) / 10))
-			return 2147483647;
-		if (sign == -1 && (result > (2147483648 - digit) / 10))
-			return -2147483648;
-
-		result = result * 10 + (*str - '0');
-		str++;
-	}
+	result = output(str, sign);
 	return (result * sign);
 }
-
-// #include "libft.h"
 
 // int main(void)
 // {
 // 	int test;
-// 	char *a = "      86";
+// 	char *a = "      86444444444444444444";
 
 // 	test = ft_atoi(a);
 // 	printf("c'est le %d\n", test);
